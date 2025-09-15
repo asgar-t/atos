fa51887b5588660abd177a69a668982b3b454033
asgar-t <asgartalajawala@gmail.com>
2025-09-15 14:45:20 -0400

    document printf.h

--- src/printf.h
+++ src/printf.h
@@ -1,5 +1,8 @@
 #pragma once
 
+//own printf function, this one was definitely fun to write as it was the first function
+//i wrote that has unknown arguments so you have to make use of a pointer to the stack, which I will highlight in the
+//documentation in printf.c
 
 //only %d (signed), %u, %c, and %s
 void __attribute__((cdecl)) printf(const char* fmt, ...);

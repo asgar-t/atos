f215a315be6a22b67cf643cd88cc995297ce022f
asgar-t <asgartalajawala@gmail.com>
2025-09-15 14:42:03 -0400

    document util.h

--- src/util.h
+++ src/util.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "stdint.h"
 
-void memset(void* dest, char val, uint32_t count);
+//useful functions
+
+void memset(void* dest, char val, uint32_t count); //sets a chunk of memory
+
+//controls ports, write a byte to a port, or read from a port
 void outPortB(uint16_t port, uint8_t value);
 char inPortB(uint16_t port);
 
+//struct containing all the registers, used for interrupts
 typedef struct interrupt_registers{
     uint32_t cr2, ds, edi, esi, ebp, esp, ebx, edx, ecx, eax, int_no, err_code, eip, csm, eflags, useresp, ss;
 

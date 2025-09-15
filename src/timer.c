831829e99f8e31bdbac97a5d1f2313587ad3bdaa
asgar-t <asgartalajawala@gmail.com>
2025-09-15 14:44:04 -0400

    document timer.c

--- src/timer.c
+++ src/timer.c
@@ -6,13 +6,16 @@
 uint64_t ticks;
 const uint32_t freq = 100;
 
+//initializes timer
 void initTimer(){
 
+    //add to irq table
     ticks = 0;
     irq_install_handler(0, onIRQ0);
 
     uint32_t divisor = 1193182/freq; //frequency in Hz (rounding)
 
+    //timer configuration
     outPortB(0x43, 0x36);
     outPortB(0x40 , (uint8_t)(divisor & 0xff));
     outPortB(0x40, (uint8_t)((divisor >> 8) & 0xff));
@@ -20,6 +23,8 @@ void initTimer(){
 
 
 }
+
+//just for testing purposes, will include timer based functionality in the future. 
 void onIRQ0(interrupt_registers* regs){
     ticks ++;
 

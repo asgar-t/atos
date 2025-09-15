4c9eb1adbe3f68622097a35bef74035d235d62a3
asgar-t <asgartalajawala@gmail.com>
2025-09-15 14:43:00 -0400

    document util.c

--- src/util.c
+++ src/util.c
@@ -1,5 +1,6 @@
 #include "util.h"
 
+//iterates through the data, setting each byte to whatever value designated
 void memset(void* dest, char val, uint32_t count){
 
     char* cur = (char*)dest;
@@ -12,6 +13,7 @@ void memset(void* dest, char val, uint32_t count){
 }
 
 
+//reading from/writing to ports
 void outPortB(uint16_t port, uint8_t value){
 
     asm volatile ("outb %1, %0" : : "dN" (port), "a" (value));
